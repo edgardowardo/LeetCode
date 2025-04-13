6cbe2acdb5a5ffd061f20f111ea3b1e9b29efbf5
Edgar Agno <edgardo76@yahoo.com>
2025-04-13 11:04:59 +0100

    Initial Commit

---
+++ LeetCode/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  LeetCode
+//
+//  Created by EDGARDO AGNO on 4/13/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ LeetCode/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by EDGARDO AGNO on 4/13/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
