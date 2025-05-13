64b1a4938ebf47dd1da28b12b54dfc4bf307a969
Edgar Agno <edgardo76@yahoo.com>
2025-05-13 14:01:55 +0100

    ListNode definition

--- DebugMe/DebugMe/AppDelegate.swift
+++ DebugMe/DebugMe/AppDelegate.swift
@@ -16,6 +16,18 @@ import HeapModule
 
 ///---------------------------------------------------------------------------------------
 ///
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+    public init(_ val: Int, _ next: ListNode?) {
+        self.val = val
+        self.next = next
+    }
+}
 
 ///---------------------------------------------------------------------------------------
 ///
