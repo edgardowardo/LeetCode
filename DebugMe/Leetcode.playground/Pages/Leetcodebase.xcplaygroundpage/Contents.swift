60525900398834cb162269e2bba357234c2713c0
Edgar Agno <edgardo76@yahoo.com>
2025-05-22 15:54:00 +0100

    Leet0454

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -18219,7 +18219,24 @@ class Leet3362 {
 }
 //Leet3362.test()
 
-
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/4sum-ii/
+class Leet0454 {
+    func fourSumCount(_ nums1: [Int], _ nums2: [Int], _ nums3: [Int], _ nums4: [Int]) -> Int {
+        var m = [Int:Int](), result = 0
+        for a in nums1 {
+            for b in nums2 {
+                m[a+b, default: 0] += 1
+            }
+        }
+        for c in nums3 {
+            for d in nums4 {
+                result += m[-c-d, default: 0]
+            }
+        }
+        return result
+    }
+}
 
 
 print("All playground tests passed!")
