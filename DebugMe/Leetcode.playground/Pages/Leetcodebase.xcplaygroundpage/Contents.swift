dc86024fa79b24b0a99b28a03cdbdf040130062b
Edgar Agno <edgardo76@yahoo.com>
2025-05-30 15:20:35 +0100

    Leet2211

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -19052,4 +19052,48 @@ class Leet0735 {
 }
 
 
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/count-collisions-on-a-road/
+class Leet2211 {
+    func countCollisions(_ directions: String) -> Int {
+        var d = Deque(directions), stack = [Character](), result = 0
+        while !d.isEmpty {
+            // "RL" becomes "S"
+            while let last = stack.last, last == "R", let f = d.first, f == "L" {
+                result += 2
+                stack.removeLast()
+                d.removeFirst()
+                d.prepend(contentsOf: "S")
+            }
+            // "RS" collision
+            while let last = stack.last, last == "R", let f = d.first, f == "S" {
+                result += 1
+                stack.removeLast()
+            }
+            // "SL collision"
+            while let last = stack.last, last == "S", let f = d.first, f  == "L" {
+                result += 1
+                d.removeFirst()
+            }
+            guard let f = d.first else { break }
+            stack.append(f)
+            d.removeFirst()
+        }
+        return result
+    }
+}
+
+
+/*
+ "RSRSLLLRSRSSRRL"
+ "RRRRRLLLLL"
+ "RLRSLL"
+ "LLRR"
+ "LLLLLLRSRSLLLRSRSSRRLRRRRRRRRRR"
+ "SSRSSRLLRSLLRSRSSRLRRRRLLRRLSSRR"
+ "LLRLRLLSLRLLSLSSSS"
+ */
+
+
+
 print("All playground tests passed!")
