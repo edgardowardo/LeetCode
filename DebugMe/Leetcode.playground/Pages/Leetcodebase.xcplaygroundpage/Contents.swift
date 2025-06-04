ff2f9612cb4200913492d5c8823958412c8bd7ff
Edgar Agno <edgardo76@yahoo.com>
2025-06-04 10:52:01 +0100

    Leet2398

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -19476,4 +19476,41 @@ class Leet2434 {
     }
 }
 
+
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/maximum-number-of-robots-within-budget/
+class Leet2398 {
+    func maximumRobots(_ chargeTimes: [Int], _ runningCosts: [Int], _ budget: Int) -> Int {
+        var l = 0, result = 0, sum = 0, deq = Deque<Int>()
+        for r in 0..<chargeTimes.count {
+            let cr = chargeTimes[r]
+            while let last = deq.last, chargeTimes[last] < cr {
+                deq.removeLast()
+            }
+            deq.append(r)
+            sum += runningCosts[r]
+            var k = r - l + 1
+            // shrink
+            while let f = deq.first, chargeTimes[f] + k * sum > budget {
+                sum -= runningCosts[l]
+                l += 1
+                k -= 1
+                if f < l {
+                    deq.removeFirst()
+                }
+            }
+            k = r - l + 1
+            result = max(result, k)
+        }
+        return result
+    }
+    static func test() {
+        let sut = Leet2398()
+        assert(sut.maximumRobots([19,63,21,8,5,46,56,45,54,30,92,63,31,71,87,94,67,8,19,89,79,25],[91,92,39,89,62,81,33,99,28,99,86,19,5,6,19,94,65,86,17,10,8,42], 85) == 1)
+    }
+}
+//Leet2398.test()
+
+
+
 print("All playground tests passed!")
