f054d241c79bf905051b194dc67ae39b9930ec92
Edgar Agno <edgardo76@yahoo.com>
2025-06-15 12:06:32 +0100

    Leet0081

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -20010,6 +20010,49 @@ class Leet0033 {
 
 
 ///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+class Leet0081 {
+    func search(_ nums: [Int], _ target: Int) -> Bool {
+        // binary search for the pivot index
+        let n = nums.count
+        var lo = 0, hi = n - 1
+        while lo <= hi {
+            let mid = lo + (hi - lo) / 2
+            
+            if nums[mid] == target {
+                return true
+            }
+
+            if nums[lo] == nums[mid], nums[mid] == nums[hi] {
+                lo += 1
+                hi -= 1
+            } else if nums[lo] <= nums[mid] {
+                if nums[lo] <= target && target < nums[mid] {
+                    hi = mid - 1
+                } else {
+                    lo = mid + 1
+                }
+            } else {
+                if nums[mid] < target && target <= nums[hi] {
+                    lo = mid + 1
+                } else {
+                    hi = mid - 1
+                }
+            }
+        }
+        return false
+    }
+    static func test() {
+        let sut = Leet0081()
+        assert(sut.search([1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1], 2))
+    }
+}
+//Leet0081.test()
+
+
+
+
+///---------------------------------------------------------------------------------------
 ///https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 class Leet0153 {
     func findMin(_ nums: [Int]) -> Int {
