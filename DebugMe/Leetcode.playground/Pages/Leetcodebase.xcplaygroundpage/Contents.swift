32c9fd88f6bfb93f30f2caa2dbcfc21e93e64ed6
Edgar Agno <edgardo76@yahoo.com>
2025-06-28 10:24:31 +0100

    Leet2099

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -21256,4 +21256,17 @@ class Leet2014 {
 }
 
 
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+class Leet2099 {
+    func maxSubsequence(_ nums: [Int], _ k: Int) -> [Int] {
+        nums
+            .enumerated()
+            .sorted { $0.element < $1.element }
+            .suffix(k)
+            .sorted { $0.offset < $1.offset }
+            .map(\.element)
+    }
+}
+
 print("All playground tests passed!")
