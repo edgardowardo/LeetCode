57d2ebb62556df1e1afb6455691702f136b9a55c
Edgar Agno <edgardo76@yahoo.com>
2025-07-13 11:44:38 +0100

    Leet0450

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -22054,4 +22054,80 @@ class Leet0250 {
 }
 
 
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/delete-node-in-a-bst/
+class Leet0450 {
+    func deleteNode(_ root: TreeNode?, _ key: Int) -> TreeNode? {
+        guard let root else { return nil }
+        
+        if key > root.val {
+            root.right = deleteNode(root.right, key)
+        } else if key < root.val {
+            root.left = deleteNode(root.left, key)
+        } else {
+            if root.left == nil, root.right == nil {
+                return nil
+            } else if root.right != nil {
+                root.val = successor(root)
+                root.right = deleteNode(root.right, root.val)
+            } else {
+                root.val = predecessor(root)
+                root.left = deleteNode(root.left, root.val)
+            }
+        }
+        return root
+    }
+    private func successor(_ node: TreeNode?) -> Int {
+        var c = node?.right
+        while c?.left != nil {
+            c = c?.left
+        }
+        guard let v = c else { return 0 }
+        return v.val
+    }
+    private func predecessor(_ node: TreeNode?) -> Int {
+        var c = node?.left
+        while c?.right != nil {
+            c = c?.right
+        }
+        guard let v = c else { return 0 }
+        return v.val
+    }
+}
+
+ 
+
+/*
+ 
+ 
+ [5,3,6,2,4,null,7]
+ 3
+ [5,3,6,2,4,null,7]
+ 0
+ []
+ 0
+ [5,2,6,null,4,null,7]
+ 0
+ [2,0,33,null,1,25,40,null,null,11,31,34,45,10,18,29,32,null,36,43,46,4,null,12,24,26,30,null,null,35,39,42,44,null,48,3,9,null,14,22,null,null,27,null,null,null,null,38,null,41,null,null,null,47,49,null,null,5,null,13,15,21,23,null,28,37,null,null,null,null,null,null,null,null,8,null,null,null,17,19,null,null,null,null,null,null,null,7,null,16,null,null,20,6]
+ 33
+ [0]
+ 0
+ [5,3,6,2,4,null,7]
+ 5
+ [5,3,6,2,4,null,7]
+ 7
+
+ 
+ 
+ 
+ [4,null,7,6,8,5,null,null,9]
+ 7
+ [1,null,2]
+ 1
+ 
+ 
+ */
+
+
+
 print("All playground tests passed!")
