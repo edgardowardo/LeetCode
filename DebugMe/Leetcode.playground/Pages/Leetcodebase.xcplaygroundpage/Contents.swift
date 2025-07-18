feeed5c1a4a5757483d6cccd214e27164f705e18
Edgar Agno <edgardo76@yahoo.com>
2025-07-18 14:14:04 +0100

    Leet0034

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -22321,4 +22321,41 @@ class Leet2906 {
  */
 
 
+
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+class Leet0034 {
+    func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
+        let first = find(nums, target, true)
+        guard first != -1 else { return [-1, -1] }
+        let last = find(nums, target, false)
+        return [first, last]
+    }
+    private func find(_ nums: [Int], _ target: Int, _ isFirst: Bool) -> Int {
+        let n = nums.count
+        var start = 0, end = n - 1
+        while start <= end {
+            let mid = start + (end - start) / 2
+            if nums[mid] == target {
+                if isFirst {
+                    if mid == start || nums[mid - 1] != target {
+                        return mid
+                    }
+                    end = mid - 1
+                } else {
+                    if mid == end || nums[mid + 1] != target {
+                        return mid
+                    }
+                    start = mid + 1
+                }
+            } else if nums[mid] > target {
+                end = mid - 1
+            } else {
+                start = mid + 1
+            }
+        }
+        return -1
+    }
+}
+
 print("All playground tests passed!")
