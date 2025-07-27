b2faab9dce27cb1ee27819e30d95b17acdd32449
Edgar Agno <edgardo76@yahoo.com>
2025-07-27 13:21:15 +0100

    Leet0162

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -22584,6 +22584,22 @@ class Leet2210 {
     }
 }
 
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/find-peak-element/
+class Leet0162 {
+    func findPeakElement(_ nums: [Int]) -> Int {
+        var l = 0, r = nums.count - 1
+        while l < r {
+            let mid = l + (r - l) / 2
+            if nums[mid] > nums[mid + 1] {
+                r = mid
+            } else {
+                l = mid + 1
+            }
+        }
+        return l
+    }
+}
 
 
 print("All playground tests passed!")
