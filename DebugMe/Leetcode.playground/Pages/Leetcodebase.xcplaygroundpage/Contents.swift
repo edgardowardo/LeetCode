54a51c67388cc0713ccad28783dd5f83f62b155d
Edgar Agno <edgardo76@yahoo.com>
2025-08-20 11:32:45 +0100

    Leet0221

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -23405,6 +23405,29 @@ class Leet1277 {
     }
 }
 
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/maximal-square/
+class Leet0221 {
+    func maximalSquare(_ matrix: [[Character]]) -> Int {
+        guard !matrix.isEmpty else { return 0 }
+        let rows = matrix.count, cols = matrix[0].count
+        var dp = [Int](repeating: 0, count: cols + 1), maxsqlen = 0, prev = 0
+        for i in 1...rows {
+            for j in 1...cols {
+                let temp = dp[j]
+                if matrix[i - 1][j - 1] == "1" {
+                    dp[j] = min(dp[j - 1], prev, dp[j]) + 1
+                    maxsqlen = max(maxsqlen, dp[j])
+                } else {
+                    dp[j] = 0
+                }
+                prev = temp
+            }
+        }
+        return maxsqlen * maxsqlen
+    }
+}
+
 
 
 print("All playground tests passed!")
