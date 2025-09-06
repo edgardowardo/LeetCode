61d3c17d1064b9defcc3d6d53f0802ffa71a89d1
Edgar Agno <edgardo76@yahoo.com>
2025-09-06 09:35:14 +0100

    Leet3495

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -23937,4 +23937,28 @@ class Leet2749 {
 }
 
 
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/minimum-operations-to-make-array-elements-zero/
+class Leet3495 {
+    func minOperations(_ queries: [[Int]]) -> Int {
+        var result = 0
+        for q in queries {
+            let c1 = get(q[1]), c2 = get(q[0] - 1)
+            result += (c1 - c2 + 1) / 2
+        }
+        return result
+    }
+    private func get(_ num: Int) -> Int {
+        var cnt = 0, i = 1, base = 1
+        while base <= num {
+            let end = min(base * 2 - 1, num)
+            cnt += (i + 1) / 2 * (end - base + 1)
+            i += 1
+            base *= 2
+        }
+        return cnt
+    }
+}
+
+
 print("All playground tests passed!")
