338a5bf2ea3f5a6ccb7b08fce2cd0a885da1f6c9
Edgar Agno <edgardo76@yahoo.com>
2025-09-10 13:57:42 +0100

    Leet1733

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -24024,40 +24024,38 @@ class Leet2327 {
 }
 
  
-/*
- 
- class Solution {
 
-     private static final int MOD = 1000000007;
-
-     public int peopleAwareOfSecret(int n, int delay, int forget) {
-         Deque<int[]> know = new LinkedList<>();
-         Deque<int[]> share = new LinkedList<>();
-         know.add(new int[] { 1, 1 });
-         int knowCnt = 1;
-         int shareCnt = 0;
-
-         for (int i = 2; i <= n; i++) {
-             if (!know.isEmpty() && know.peekFirst()[0] == i - delay) {
-                 int[] first = know.pollFirst();
-                 knowCnt = (knowCnt - first[1] + MOD) % MOD;
-                 shareCnt = (shareCnt + first[1]) % MOD;
-                 share.add(first);
-             }
-             if (!share.isEmpty() && share.peekFirst()[0] == i - forget) {
-                 int[] first = share.pollFirst();
-                 shareCnt = (shareCnt - first[1] + MOD) % MOD;
-             }
-             if (!share.isEmpty()) {
-                 knowCnt = (knowCnt + shareCnt) % MOD;
-                 know.add(new int[] { i, shareCnt });
-             }
-         }
-         return (knowCnt + shareCnt) % MOD;
-     }
- }
- 
- */
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/minimum-number-of-people-to-teach/
+class Leet1733 {
+    func minimumTeachings(_ n: Int, _ languages: [[Int]], _ friendships: [[Int]]) -> Int {
+        var cncon: Set<Int> = []
+        for f in friendships {
+            var m: [Int: Int] = [:], conm = false
+            for l in languages[f[0] - 1] {
+                m[l] = 1
+            }
+            for l in languages[f[1] - 1] {
+                if let _ = m[l] {
+                    conm = true
+                    break
+                }
+            }
+            if (!conm) {
+                cncon.insert(f[0] - 1)
+                cncon.insert(f[1] - 1)
+            }
+        }
+        var maxCount = 0, counts = [Int](repeating: 0, count: n + 1)
+        for f in cncon {
+            for l in languages[f] {
+                counts[l] += 1
+                maxCount = max(maxCount, counts[l])
+            }
+        }
+        return cncon.count - maxCount
+    }
+}
 
 
 
