3a0189d6fab188d34995cd682b3589713f56046b
Edgar Agno <edgardo76@yahoo.com>
2026-06-17 11:36:31 +0100

    Leet2839

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -25157,4 +25157,29 @@ class Leet2491 {
     }
 }
  
+
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/
+class Leet2839 {
+    func canBeEqual(_ s1: String, _ s2: String) -> Bool {
+        let s1 = Array(s1), s2 = Array(s2)
+        return s1 == s2
+        ||
+        s1[0] == s2[2] &&
+        s1[2] == s2[0] &&
+        s1[1] == s2[3] &&
+        s1[3] == s2[1]
+        ||
+        s1[0] == s2[0] &&
+        s1[2] == s2[2] &&
+        s1[1] == s2[3] &&
+        s1[3] == s2[1]
+        ||
+        s1[1] == s2[1] &&
+        s1[3] == s2[3] &&
+        s1[0] == s2[2] &&
+        s1[2] == s2[0]
+    }
+}
+
 print("All playground tests passed!")
