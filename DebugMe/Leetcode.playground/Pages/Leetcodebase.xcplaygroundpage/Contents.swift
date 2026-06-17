56ec07c61d5be733916b0934bbe115d5f4918379
Edgar Agno <edgardo76@yahoo.com>
2026-06-17 14:50:05 +0100

    Leet3065

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -25324,5 +25324,13 @@ class Leet2798 {
     }
 }
 
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/minimum-operations-to-exceed-threshold-value-i/
+class Leet3065 {
+    func minOperations(_ nums: [Int], _ k: Int) -> Int {
+        nums.reduce(0) { $0 + ($1 < k ? 1 : 0) }
+    }
+}
+
 
 print("All playground tests passed!")
