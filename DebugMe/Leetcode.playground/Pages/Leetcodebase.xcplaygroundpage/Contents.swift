c54b2f53934c69c8b21ed92d97931a916e266975
Edgar Agno <edgardo76@yahoo.com>
2026-06-18 10:55:17 +0100

    Leet2591

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -25388,5 +25388,23 @@ class Leet1822 {
     }
 }
 
+///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/distribute-money-to-maximum-children/
+class Leet2591 {
+    func distMoney(_ money: Int, _ children: Int) -> Int {
+        var money = money
+        money -= children
+        if money < 0 {
+            return -1
+        }
+        if money / 7 == children, money % 7 == 0 {
+            return children
+        }
+        if money / 7 == children - 1, money % 7 == 3 {
+            return children - 2
+        }
+        return min(children - 1, money / 7)
+    }
+}
 
 print("All playground tests passed!")
