0aa2ae6468459e04bb328aa50c5f6a63267eb876
Edgar Agno <edgardo76@yahoo.com>
2026-07-09 17:55:37 +0100

    Leet0005

--- DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
+++ DebugMe/Leetcode.playground/Pages/Leetcodebase.xcplaygroundpage/Contents.swift
@@ -21726,6 +21726,36 @@ class Leet005 {
 }
 
 ///---------------------------------------------------------------------------------------
+///https://leetcode.com/problems/longest-palindromic-substring/
+class Leet0005 {
+    func longestPalindrome(_ s: String) -> String {
+        var result = ""
+        let s = Array(s)
+        for i in 0..<s.count {
+            let oddPalindrome = longestPalindromeFrom(left: i, right: i, s: s)
+            if result.count < oddPalindrome.count {
+                result = oddPalindrome
+            }
+            let evenPalindrome = longestPalindromeFrom(left: i, right: i + 1, s: s)
+            if result.count < evenPalindrome.count {
+                result = evenPalindrome
+            }
+        }
+        
+        return result
+    }
+    
+    private func longestPalindromeFrom(left: Int, right: Int, s: Array<Character>) -> String {
+        var left = left, right = right
+        while left >= 0, right < s.count, s[left] == s[right] {
+            left -= 1
+            right += 1
+        }
+        return String(s[left + 1..<right])
+    }
+}
+
+///---------------------------------------------------------------------------------------
 ///https://leetcode.com/problems/palindromic-substrings/
 class Leet0647 {
     func countSubstrings(_ s: String) -> Int {
